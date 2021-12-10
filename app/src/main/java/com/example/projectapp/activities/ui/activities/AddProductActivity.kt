c13772823fddd665cf70ad2d0703b94dd798b4e5
rafasu4 <raf59101@gmail.com>
2021-12-10 14:15:42 +0200

    dashboard touch-up

---
+++ app/src/main/java/com/example/projectapp/activities/ui/activities/AddProductActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projectapp.activities.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectapp.R
+
+class AddProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_product)
+    }
+}
--- app/src/main/java/com/example/projectapp/activities/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/com/example/projectapp/activities/ui/fragments/ProductsFragment.kt
@@ -1,13 +1,14 @@
 package com.example.projectapp.activities.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.projectapp.R
+import com.example.projectapp.activities.ui.activities.AddProductActivity
 import com.example.projectapp.activities.ui.products.ProdyctsViewModel
 import com.example.projectapp.databinding.FragmentProductsBinding
 
@@ -21,6 +22,10 @@ class ProductsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true) //Enabling the option menu
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,6 +42,28 @@ class ProductsFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    /**
+     * Adds the 'Add Product' button to the fragment view.
+     */
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_product_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    /**
+     * Sets the action of the 'Add Product' button
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId //the id of the pressed item
+        when(id){
+            R.id.action_add_product ->{
+                startActivity(Intent(activity, AddProductActivity::class.java ))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {
