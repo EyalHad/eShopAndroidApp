e15af8437f0f8a857d4e2f268ba3470a2d354540
yehonadav yadai <hvubsc222@gmail.com>
2021-12-15 01:11:46 +0200

    added images to products view

--- app/src/main/java/com/example/projectapp/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/com/example/projectapp/ui/fragments/ProductsFragment.kt
@@ -5,6 +5,7 @@ import android.app.ActionBar
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -15,7 +16,8 @@ import com.example.projectapp.databinding.FragmentProductsBinding
 import com.example.projectapp.ui.activities.AddProductActivity
 import com.example.projectapp.ui.view_models.ProductsViewModel
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.fragment_products.*
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.QueryDocumentSnapshot
 
 
 class ProductsFragment : Fragment() {
@@ -92,12 +94,14 @@ class ProductsFragment : Fragment() {
 
     /**
      * Read every product make a dynmic layout
+     * of text view(for every attribute)
+
      */
     fun readFireStoreData(root: View) {
         val db = FirebaseFirestore.getInstance()
         //this is the field will be check for now
-        val fields = arrayOf("name", "price")
-        db.collection("check")
+        val fields = arrayOf("title", "price","stock_quantity","description")
+        db.collection("Products")
             .get()
             .addOnCompleteListener {
 
@@ -110,6 +114,7 @@ class ProductsFragment : Fragment() {
 
                             //result.append(document.toString()).append("\n\n")
                             if (document.contains(f)) {
+
                                 result.append(f).append(" : ").append(document.data.getValue(f))
                                     .append("\n")
                             }
@@ -118,7 +123,10 @@ class ProductsFragment : Fragment() {
                         }
 
                         add_text_view_for_doc(result,root)
-                        //here we will make function to add image .
+                        //now i will add the image of this document
+                        add_image_view_for_doc(document,root)
+
+
 
 
                     }
@@ -126,6 +134,20 @@ class ProductsFragment : Fragment() {
                 }
             }
     }
+    /**
+     * build Image view for document
+     */
+    private fun add_image_view_for_doc(document: QueryDocumentSnapshot, root: View) {
+        if (document.contains("image")) {
+            val pl: View = root.findViewById(R.id.main_layout)
+            val image_view = ImageView(pl.context)
+            Glide.with(this)
+                .load(document.data.getValue("image").toString())
+                .into(image_view)
+            (pl as LinearLayout).addView(image_view)
+        }
+    }
+
     /**
      * build text view for the string we made from the product document
      */
