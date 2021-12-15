8ab7f0f9979b8993490405a6f4d82a60dc1781f8
rafasu4 <raf59101@gmail.com>
2021-12-15 16:38:26 +0200

    client shopping menu editted

--- app/src/main/java/com/example/projectapp/ui/activities/ClientShoppingActivity.kt
+++ app/src/main/java/com/example/projectapp/ui/activities/ClientShoppingActivity.kt
@@ -1,5 +1,6 @@
 package com.example.projectapp.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,7 +11,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.example.projectapp.R
 import com.google.android.material.navigation.NavigationView
 
-class ClientShoppingActivity : AppCompatActivity() {
+class ClientShoppingActivity : BaseActivity() {
 
     lateinit var toggle : ActionBarDrawerToggle //menu map toggle
 
@@ -27,7 +28,12 @@ class ClientShoppingActivity : AppCompatActivity() {
         //what to do on each item in the menu
         navView.setNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.nav_home -> Toast.makeText(this, "click!", Toast.LENGTH_SHORT).show()
+                R.id.nav_home -> Toast.makeText(this, "Home Clicked!", Toast.LENGTH_SHORT).show()
+                R.id.nav_my_orders -> Toast.makeText(this, "My Orders Clicked!", Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(this, "Settings Clicked!", Toast.LENGTH_SHORT).show()
+                R.id.nav_edit_profile -> Toast.makeText(this, "Edit Profile Clicked!", Toast.LENGTH_SHORT).show()
+                R.id.nav_login -> startActivity(Intent(this, SignInActivity::class.java))
+                R.id.nav_admin -> Toast.makeText(this, "Admin Clicked!", Toast.LENGTH_SHORT).show()
             }
             true
         }
@@ -38,6 +44,10 @@ class ClientShoppingActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(toggle.onOptionsItemSelected(item)){return true}
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        doubleBackToExit()
     }
 
 
